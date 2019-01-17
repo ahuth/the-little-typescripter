8986576e66f10366b724d239ffb47ec9b9e2a9cf
Andrew Huth <andrew@huth.me>
2019-01-17 07:42:09 -0800

    Add more type-safe list
    At the expense of a lot of verbosity. Not sure if there's a better way, but it's challenging because lists can have all sorts of different data types, AND are nested Cons cells.
    
    Also note that because lists can theoretically be the empty list, I have to handle that case in `getHead` and `getTail`.

--- src/list.ts
+++ src/list.ts
@@ -7,13 +7,33 @@ import {
   Cons,
 } from './cons';
 
-type List = Cons<any, any>;
+type FullList = Cons<any, any>;
+type EmptyList = [];
+type List = FullList | EmptyList;
 
-export const getHead = car;
-export const getTail = cdr;
-
-export function list(...items: Array<any>): List {
+export function list<A, B, C, D, E, F, G, H, I, J>(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, Cons<F, Cons<G, Cons<H, Cons<I, Cons<J, EmptyList>>>>>>>>>>;
+export function list<A, B, C, D, E, F, G, H, I>(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, Cons<F, Cons<G, Cons<H, Cons<I, EmptyList>>>>>>>>>;
+export function list<A, B, C, D, E, F, G, H>(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, Cons<F, Cons<G, Cons<H, EmptyList>>>>>>>>;
+export function list<A, B, C, D, E, F, G>(a: A, b: B, c: C, d: D, e: E, f: F, g: G): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, Cons<F, Cons<G, EmptyList>>>>>>>;
+export function list<A, B, C, D, E, F>(a: A, b: B, c: C, d: D, e: E, f: F): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, Cons<F, EmptyList>>>>>>;
+export function list<A, B, C, D, E>(a: A, b: B, c: C, d: D, e: E): Cons<A, Cons<B, Cons<C, Cons<D, Cons<E, EmptyList>>>>>;
+export function list<A, B, C, D>(a: A, b: B, c: C, d: D): Cons<A, Cons<B, Cons<C, Cons<D, EmptyList>>>>;
+export function list<A, B, C>(a: A, b: B, c: C): Cons<A, Cons<B, Cons<C, EmptyList>>>;
+export function list<A, B>(a: A, b: B): Cons<A, Cons<B, EmptyList>>;
+export function list<A>(a: A): Cons<A, EmptyList>;
+export function list(): EmptyList;
+export function list(...items: Array<any>) {
   return items.reduceRight((acc, item) => cons(item, acc), []);
+}
+
+export function getHead(x: List): any {
+  if (isEmpty(x)) { return undefined; }
+  return car(x as FullList);
+}
+
+export function getTail(x: List): List {
+  if (isEmpty(x)) { return []; }
+  return cdr(x as FullList);
 }
 
 export function toString(l: List, acc = '('): string {
