3441790fd20c29ff814b049c461b1ef22f132e2e
Andrew Huth <andrew@huth.me>
2019-01-16 20:41:38 -0800

    Add "toString" for lists and expand tests to take advantage

--- src/list.test.ts
+++ src/list.test.ts
@@ -1,6 +1,11 @@
-import { list, nth } from './list';
+import { list, getHead, getTail, nth, toString } from './list';
 
 test('lists', () => {
+  const l = list(1, 1, 2, 3, 5, 8);
+  expect(toString(l)).toEqual('(1 1 2 3 5 8)');
+});
+
+test('nth', () => {
   const l = list(1, 1, 2, 3, 5, 8);
   expect(nth(l, 0)).toEqual(1);
   expect(nth(l, 1)).toEqual(1);
@@ -8,4 +13,10 @@ test('lists', () => {
   expect(nth(l, 3)).toEqual(3);
   expect(nth(l, 4)).toEqual(5);
   expect(nth(l, 5)).toEqual(8);
+});
+
+test('head and tail', () => {
+  const l = list(2, 4, 6, 8);
+  expect(getHead(l)).toEqual(2);
+  expect(toString(getTail(l))).toEqual('(4 6 8)');
 });
--- src/list.ts
+++ src/list.ts
@@ -1,10 +1,21 @@
-import { cons, car, cdr } from './cons';
+import { cons, car, cdr, isAtom, isEmpty } from './cons';
 
 export const getHead = car;
 export const getTail = cdr;
 
 export function list(...items: Array<any>): [any, any] {
   return items.reduceRight((acc, item) => cons(item, acc), []);
+}
+
+export function toString(l: [any, any], acc = '('): string {
+  if (isEmpty(l)) { return acc + ')'; }
+  if (isAtom(l)) { return String(l); }
+
+  const head = getHead(l);
+  const tail = getTail(l);
+  const trailing = isEmpty(tail) ? '' : ' ';
+
+  return toString(tail, acc + toString(head) + trailing);
 }
 
 export function nth(l: [any, any], n: number): any {
