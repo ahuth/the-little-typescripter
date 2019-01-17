43400b8b139b0a8aac4fa0f35dc66dad5db28cc9
Andrew Huth <andrew@huth.me>
2019-01-16 20:32:59 -0800

    Add list selectors which make their usage more clear
    "head" is more clear than "car" and "tail" more clear than "cdr" for people less familiar with Lisp/Scheme.

--- src/list.ts
+++ src/list.ts
@@ -1,12 +1,15 @@
 import { cons, car, cdr } from './cons';
 
+export const getHead = car;
+export const getTail = cdr;
+
 export function list(...items: Array<any>): [any, any] {
   return items.reduceRight((acc, item) => cons(item, acc), []);
 }
 
 export function nth(l: [any, any], n: number): any {
   if (n <= 0) {
-    return car(l);
+    return getHead(l);
   }
-  return nth(cdr(l), n - 1);
+  return nth(getTail(l), n - 1);
 }
