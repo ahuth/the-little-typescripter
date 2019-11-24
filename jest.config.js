cdfd40d5349ab9214c67338aeec5e284bdb45079
Andrew Huth <andrew@huth.me>
2019-11-24 07:43:28 -0800

    Extract Jest config to a config file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest'
+  }
+}
